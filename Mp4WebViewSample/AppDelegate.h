a9fa6e778fabffd070969f6a875a310f5a41ad5b
Kevin <gr20060513@gmail.com>
2016-10-27 10:47:44 +0800

    Initial Commit

---
+++ Mp4WebViewSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mp4WebViewSample
+//
+//  Created by ctitv on 2016/10/27.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mp4WebViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mp4WebViewSample
+//
+//  Created by ctitv on 2016/10/27.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
